b9c61ba483e463e10aefd73060c261733672db07
khmerak <141410660+khmerak@users.noreply.github.com>
2025-06-16 15:34:32 +0700

    h

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://a.khmercleaningservice.us/api",
+  baseURL: "https://a.khmercleaningservice.us/",
 });
 
 api.interceptors.request.use((config) => {
